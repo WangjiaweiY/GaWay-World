8d82902771c316d692a2c4c2021eab94429a9e84
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2023-08-09 17:35:19 +0800

    2.新增了登录页面

---
+++ src/login/index.js
@@ -0,0 +1,19 @@
+document.querySelector(".btn").addEventListener("click", ()=> {
+    login()
+    console.log("登录点击成功")
+})
+
+function login() {
+    // 获取用户名和密码输入框的值
+    var username = document.querySelector("input[name='username']").value;
+    var password = document.querySelector("input[name='password']").value;
+    // 假设正确的用户名和密码为 "admin" 和 "password"
+    var correctUsername = "admin";
+    var correctPassword = "password";
+    // 验证用户名和密码是否正确
+    if (username === correctUsername && password === correctPassword) {
+        console.log("登录成功");
+    } else {
+        console.log("用户名或密码错误");
+    }
+}
