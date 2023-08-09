0d9aee2fbadeceaffce80cecb79e5c263c7a7334
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2023-08-09 18:14:36 +0800

    登录跳转爱心

--- src/login/index.js
+++ src/login/index.js
@@ -1,7 +1,5 @@
 document.querySelector(".btn").addEventListener("click", ()=> {
     login()
-    console.log("登录点击成功")
-    window.location.href = "https://www.baidu.com";
 })
 
 function login() {
@@ -9,11 +7,12 @@ function login() {
     var username = document.querySelector("input[name='username']").value;
     var password = document.querySelector("input[name='password']").value;
     // 假设正确的用户名和密码为 "admin" 和 "password"
-    var correctUsername = "admin";
-    var correctPassword = "password";
+    var correctUsername = "yinyutong";
+    var correctPassword = "yytwjw1314";
     // 验证用户名和密码是否正确
     if (username === correctUsername && password === correctPassword) {
         console.log("登录成功");
+        window.location.href = "../snow-flowers/index.html";
     } else {
         console.log("用户名或密码错误");
     }
