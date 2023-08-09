10638187d26066d55b8252c0ade97486b2c804b9
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2023-08-09 18:25:39 +0800

    修改bug

--- src/login/index.js
+++ src/login/index.js
@@ -12,7 +12,7 @@ function login() {
     // 验证用户名和密码是否正确
     if (username === correctUsername && password === correctPassword) {
         console.log("登录成功");
-        window.location.href = "../snow-flowers/index.html";
+        window.location.href = "./index1.html";
     } else {
         console.log("用户名或密码错误");
     }
