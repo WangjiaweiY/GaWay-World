8f93aa8cc2e102d2468097a4aff244bea95257c2
WangjiaweiY <127864206+WangjiaweiY@users.noreply.github.com>
2023-08-10 22:08:02 +0800

    新增登录成功和失败样式，新增content页面

---
+++ src/content/index.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn").addEventListener( 'click', ()=>{
+    console.log(123)
+    window.location.href = "../snow-flowers/index.html"
+} )
--- src/login/index.js
+++ src/login/index.js
@@ -10,10 +10,23 @@ function login() {
     var correctUsername = "yinyutong";
     var correctPassword = "yytwjw1314";
     // 验证用户名和密码是否正确
+    const TX = document.querySelector('.tixing-container')
+    const TX_TEXT = document.querySelector('.tixing')
     if (username === correctUsername && password === correctPassword) {
         console.log("登录成功");
-        window.location.href = "./index1.html";
+        TX.style.visibility = "visible"
+        TX_TEXT.innerHTML = "登录成功"
+        TX.style.backgroundColor = 'lightgreen';
+        setTimeout( ()=>{
+            window.location.href = "../content/index.html";
+        },1000)
     } else {
+        TX.style.visibility = "visible"
+        TX_TEXT.innerHTML = "用户名或密码错误"
+        TX.style.backgroundColor = 'red';
+        setTimeout( ()=> {
+            TX.style.visibility = "hidden"
+        },1000)
         console.log("用户名或密码错误");
     }
 }
